e4baed77bca1a6a9d2c2ea979a7f147667e163da
brint <brint@blendimc.com>
2014-09-27 13:29:37 -0500

    Icomoon Updates

--- controller/get-unijoules.php
+++ controller/get-unijoules.php
@@ -3,24 +3,24 @@
 // Make sure you're logged in, otherwise return home
 if(!Me::$loggedIn)
 {
-	Me::redirectLogin("/get-credits");
+	Me::redirectLogin("/get-unijoules");
 }
 
 // Run Global Script
 require(APP_PATH . "/includes/global.php");
 
-// Provide Free Credits (only available to local and development)
+// Provide Free UniJoule (only available to local and development)
 if(isset($_GET['alpha']) and $_GET['alpha'] == "free" and ENVIRONMENT != "production")
 {
 	if($balance < 9600)
 	{
-		AppTransactions::add(Me::$vals['auth_id'], Me::$id, 500.00, "Free Credits for Pre-Release");
+		AppTransactions::add(Me::$vals['auth_id'], Me::$id, 500.00, "Free UniJoule for Pre-Release");
 		
-		Alert::saveSuccess("Free Credits", 'Free windfall! Pre-release is great! Enjoy your credits!');
+		Alert::saveSuccess("Free UniJoule", 'Free windfall! Pre-release is great! Enjoy your UniJoule!');
 	}
 	else
 	{
-		Alert::saveSuccess("Enough Credits", "Okay, okay, let's not get crazy now.");
+		Alert::saveSuccess("Enough UniJoule", "Okay, okay, let's not get crazy now.");
 	}
 	
 	header("Location: /"); exit;
@@ -38,11 +38,11 @@ echo '
 <div id="panel-right"></div>
 <div id="content">' . Alert::display() . '
 
-<h2 style="font-weight:normal;">Buy Credits &nbsp; &nbsp; <span style="color:#57c2c1;">$1.00 = 1.00 Credits</span></h2>
+<h2 style="font-weight:normal;">Buy UniJoule &nbsp; &nbsp; <span style="color:#57c2c1;">$1.00 = 1.00 UniJoule</span></h2>
 
 <p>
-	5% free credits with $20 or more<br />
-	10% free credits with $50 or more
+	Get 5% free UniJoule with $20 or more<br />
+	Get 10% free UniJoule with $50 or more
 </p>
 
 <img src="/assets/images/paypal_cards.png" />
